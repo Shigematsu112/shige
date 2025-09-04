8e5bf7eca63789801e2e78f9f51f236642478d9b
kuruk <kuruk@192.168.0.6>
2025-09-04 15:27:25 +0900

    課題

---
+++ HelloWorld_shigematsukurumi/src/animal/Dog.java
@@ -0,0 +1,21 @@
+package animal;
+
+public class Dog {
+	String name;// Q1：フィールドに動物の名前の変数を定義してください。
+	
+	int age;// Q2：フィールドに動物の数の変数を定義してください。
+	
+
+	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
+    public Dog(){
+        this.name="犬";
+    }
+	
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+    public Dog(int age){
+        this.age=age;
+}
+
+
+
+}
